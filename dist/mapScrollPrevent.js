a9aa9369f42e59eeaf7a7ece1250fd8db3a15861
Emiliano Díaz <diazemiliano.personal@gmail.com>
2015-08-26 18:20:06 -0300

    Print Log

--- dist/mapScrollPrevent.js
+++ dist/mapScrollPrevent.js
@@ -10,7 +10,7 @@
 (function($) {
     return $.fn.extend({
         mapScrollPrevent: function(options) {
-            var applyCss, coverObject, defaults, hideOverlay, iframeObject, isTouchScreen, mapCSS, opts, overlayObject, showOverlay, wrapIframe, wrapObject;
+            var Log, applyCss, bindEvents, coverObject, defaults, hideOverlay, iframeObject, isTouchScreen, mapCSS, opts, overlayObject, showOverlay, wrapIframe, wrapObject;
             defaults = {
 
                 /* Custom class for map wrap */
@@ -20,93 +20,121 @@
                 overlayClass: "map-overlay",
 
                 /* Hover Message */
-                overlayMessage: "Clic para Navegar."
+                overlayMessage: "Clic para Navegar.",
+
+                /* Print Log Messges */
+                printLog: false
             };
             opts = $.extend(true, defaults, options);
-            mapCSS = "/* --- mapScrollPrevent.js CSS Classes --- */ ." + opts.overlayClass + " { position: absolute; overflow:hidden; cursor: pointer; text-align: center; background-color: rgba(255, 255, 255, 0); } ." + opts.overlayClass + ", ." + opts.overlayClass + " p { -moz-transition: all .3s ease-in-out; -o-transition: all .3s ease-in-out; -webkit-transition: all .3s ease-in-out; transition: all .3s ease-in-out; } ." + opts.overlayClass + ":hover { background-color: rgba(255, 255, 255, 0.8); } ." + opts.overlayClass + " p { font-family: Lato, 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 13px; padding-top: 2.5%; padding-bottom: 2.5%; margin-right: auto; margin-left: auto; width: 70%; position: relative; top: 50%; transform: translateY(-50%); border-color: rgba(0, 0, 0, 0.3); color: rgba(58, 132, 223, 0); background-color: rgba(0, 0, 0, 0); -moz-border-radius-topleft: 2px; -webkit-border-top-left-radius: 2px; border-top-left-radius: 2px; -moz-border-radius-topright: 2px; -webkit-border-top-right-radius: 2px; border-top-right-radius: 2px; } ." + opts.overlayClass + ":hover p { background-color: rgb(255, 255, 255); color: rgb(58, 132, 223); -moz-box-shadow: rgba(0,0,0,0.3) 0px 1px 4px -1px; -webkit-box-shadow: rgba(0, 0, 0, 0.3) 0px 1px 4px -1px; box-shadow: rgba(0, 0, 0, 0.3) 0px 1px 4px -1px; } ." + opts.wrapClass + " { display: inline-block; } ." + opts.wrapClass + " iframe { position: relative; top: 0; left: 0; }";
 
             /* iframe Map Object */
             iframeObject = $(this);
-
-            /* Creates overlay object */
-            overlayObject = $("<div class=\"" + opts.overlayClass + "\"> <p>" + opts.overlayMessage + "</p></div>");
-            wrapObject = $("<div class=\"" + opts.wrapClass + "\"></div>");
+            Log = function(message) {
+                var dateTime, nowTime;
+                if (opts.printLog) {
+                    dateTime = new Date();
+                    nowTime = dateTime.getHours() + ":" + dateTime.getMinutes() + ":" + dateTime.getSeconds();
+                    return console.log("mapScrollPrevent [" + nowTime + "] : " + message);
+                }
+            };
 
             /* Early exit */
             if (!iframeObject.length) {
-                return;
-            }
+                return Log("No Iframes detected. Try changing your \"selector.\"");
+            } else {
+                Log(iframeObject.length + " iFrames detected.");
+                mapCSS = "/* --- mapScrollPrevent.js CSS Classes --- */ ." + opts.overlayClass + " { position: absolute; overflow:hidden; cursor: pointer; text-align: center; background-color: rgba(255, 255, 255, 0); } ." + opts.overlayClass + ", ." + opts.overlayClass + " p { -moz-transition: all .3s ease-in-out; -o-transition: all .3s ease-in-out; -webkit-transition: all .3s ease-in-out; transition: all .3s ease-in-out; } ." + opts.overlayClass + ":hover { background-color: rgba(255, 255, 255, 0.8); } ." + opts.overlayClass + " p { font-family: Lato, 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 13px; padding-top: 2.5%; padding-bottom: 2.5%; margin-right: auto; margin-left: auto; width: 70%; position: relative; top: 50%; transform: translateY(-50%); border-color: rgba(0, 0, 0, 0.3); color: rgba(58, 132, 223, 0); background-color: rgba(0, 0, 0, 0); -moz-border-radius-topleft: 2px; -webkit-border-top-left-radius: 2px; border-top-left-radius: 2px; -moz-border-radius-topright: 2px; -webkit-border-top-right-radius: 2px; border-top-right-radius: 2px; } ." + opts.overlayClass + ":hover p { background-color: rgb(255, 255, 255); color: rgb(58, 132, 223); -moz-box-shadow: rgba(0,0,0,0.3) 0px 1px 4px -1px; -webkit-box-shadow: rgba(0, 0, 0, 0.3) 0px 1px 4px -1px; box-shadow: rgba(0, 0, 0, 0.3) 0px 1px 4px -1px; } ." + opts.wrapClass + " { display: inline-block; } ." + opts.wrapClass + " iframe { position: relative; top: 0; left: 0; }";
 
-            /* Wraps the iframe */
-            wrapIframe = function() {
+                /* Creates overlay object */
+                overlayObject = $("<div class=\"" + opts.overlayClass + "\"> <p>" + opts.overlayMessage + "</p></div>");
+                wrapObject = $("<div class=\"" + opts.wrapClass + "\"></div>");
 
-                /* Check first if the iframe is already wraped */
-                if (!iframeObject.closest("." + opts.wrapClass).is("div")) {
-                    iframeObject.wrap(wrapObject);
-                }
+                /* Wraps the iframe */
+                wrapIframe = function() {
 
-                /* Update variable objects with DOM objects */
-                wrapObject = iframeObject.closest("." + opts.wrapClass).append(overlayObject);
-                overlayObject = wrapObject.children("." + opts.overlayClass);
-                return coverObject();
-            };
+                    /* Check first if the iframe is already wraped */
+                    if (!iframeObject.closest("." + opts.wrapClass).is("div")) {
+                        iframeObject.wrap(wrapObject);
+                        Log("Iframe isn't wraped.");
+                    }
 
-            /* Apply all the css */
-            applyCss = function() {
-                return $("head").append("<style rel=\"stylesheet\" type=\"text/css\"> " + mapCSS + "</style>");
-            };
-            coverObject = function() {
-                return overlayObject.height(iframeObject.height()).width(iframeObject.width()).css({
-                    "top": iframeObject.position().top,
-                    "left": iframeObject.position().left
-                });
-            };
+                    /* Update variable objects with DOM objects */
+                    wrapObject = iframeObject.closest("." + opts.wrapClass).append(overlayObject);
+                    overlayObject = wrapObject.children("." + opts.overlayClass);
+                    coverObject();
+                    return Log("Iframe now wraped.");
+                };
 
-            /* Overlay functions */
-            hideOverlay = function() {
-                coverObject();
-                iframeObject.css({
-                    "pointer-events": "auto"
-                });
-                return overlayObject.hide();
-            };
-            showOverlay = function() {
-                coverObject();
-                iframeObject.css({
-                    "pointer-events": "none"
-                });
-                return overlayObject.show();
-            };
+                /* Apply all the css */
+                applyCss = function() {
+                    $("head").append("<style rel=\"stylesheet\" type=\"text/css\"> " + mapCSS + "</style>");
+                    return Log("Plugin css applied.");
+                };
+                coverObject = function() {
+                    overlayObject.height(iframeObject.height()).width(iframeObject.width()).css({
+                        "top": iframeObject.position().top,
+                        "left": iframeObject.position().left
+                    });
+                    return Log("Overlay positioned.");
+                };
 
-            /* Check TouchScreen */
-            isTouchScreen = function() {
-                if ($(window).bind("touchstart")) {
-                    return true;
-                }
-            };
-            return {
+                /* Overlay functions */
+                hideOverlay = function() {
+                    iframeObject.css({
+                        "pointer-events": "auto"
+                    });
+                    overlayObject.hide();
+                    return Log("Overlay is hidden.");
+                };
+                showOverlay = function() {
+                    iframeObject.css({
+                        "pointer-events": "none"
+                    });
+                    overlayObject.show();
+                    return Log("Overlay is showed.");
+                };
 
-                /* Init wrap and bind events */
-                start: function() {
-                    applyCss();
-                    wrapIframe();
+                /* Check TouchScreen */
+                isTouchScreen = function() {
+                    if ($(window).bind("touchstart")) {
+                        Log("Is a touch screen.");
+                        return true;
+                    }
+                };
+
+                /* Bind Events */
+                bindEvents = function() {
 
                     /* Dynamic Adjust */
                     $(window).on("resize", coverObject);
                     iframeObject.on("resize", coverObject);
                     overlayObject.bind("click", hideOverlay);
-                    return wrapObject.bind("mouseleave", hideOverlay).bind("mouseenter", showOverlay);
-                },
+                    wrapObject.bind("mouseleave", hideOverlay).bind("mouseenter", showOverlay);
+                    return Log("Events bounded.");
+                };
+                return {
 
-                /* Removes everithing */
-                stop: function() {
-                    iframeObject.removeAttr("style");
-                    if (iframeObject.parent().is("." + opts.wrapClass)) {
-                        iframeObject.unwrap();
+                    /* Init wrap and bind events */
+                    start: function() {
+                        Log("Starting plugin...");
+                        applyCss();
+                        wrapIframe();
+                        bindEvents();
+                        return Log("Plugin Started.");
+                    },
+
+                    /* Removes everything */
+                    stop: function() {
+                        Log("Stopping plugin...");
+                        iframeObject.removeAttr("style");
+                        if (iframeObject.parent().is("." + opts.wrapClass)) {
+                            iframeObject.unwrap();
+                        }
+                        $("." + opts.overlayClass).remove();
+                        return Log("Plugin Stopped.");
                     }
-                    return $("." + opts.overlayClass).remove();
-                }
-            };
+                };
+            }
         }
     });
 })(jQuery);
--- src/mapScrollPrevent.coffee
+++ src/mapScrollPrevent.coffee
@@ -15,154 +15,181 @@ do ($ = jQuery) ->
     mapScrollPrevent : (options) ->
       defaults =
         ### Custom class for map wrap ###
-        wrapClass:"map-wrap"
+        wrapClass: "map-wrap"
         ### Custom class for hover div###
-        overlayClass:"map-overlay"
+        overlayClass: "map-overlay"
         ### Hover Message ###
-        overlayMessage:"Clic para Navegar."
+        overlayMessage: "Clic para Navegar."
+        ### Print Log Messges ###
+        printLog: false
 
       opts = $.extend true, defaults, options
-
-      mapCSS = "
-        /* --- mapScrollPrevent.js CSS Classes --- */
-        .#{ opts.overlayClass } {
-          position: absolute;
-          overflow:hidden;
-          cursor: pointer;
-          text-align: center;
-          background-color: rgba(255, 255, 255, 0);
-        }
-        .#{ opts.overlayClass },
-        .#{ opts.overlayClass } p {
-          -moz-transition: all .3s ease-in-out;
-          -o-transition: all .3s ease-in-out;
-          -webkit-transition: all .3s ease-in-out;
-          transition: all .3s ease-in-out;
-        }
-        .#{ opts.overlayClass }:hover {
-          background-color: rgba(255, 255, 255, 0.8);
-        }
-        .#{ opts.overlayClass } p {
-          font-family: Lato, 'Helvetica Neue', Helvetica, Arial, sans-serif;
-          font-size: 13px;
-          padding-top: 2.5%;
-          padding-bottom: 2.5%;
-          margin-right: auto;
-          margin-left: auto;
-          width: 70%;
-          position: relative;
-          top: 50%;
-          transform: translateY(-50%);
-          border-color: rgba(0, 0, 0, 0.3);
-          color: rgba(58, 132, 223, 0);
-          background-color: rgba(0, 0, 0, 0);
-          -moz-border-radius-topleft: 2px;
-          -webkit-border-top-left-radius: 2px;
-          border-top-left-radius: 2px;
-          -moz-border-radius-topright: 2px;
-          -webkit-border-top-right-radius: 2px;
-          border-top-right-radius: 2px;
-        }
-        .#{ opts.overlayClass }:hover p {
-          background-color: rgb(255, 255, 255);
-          color: rgb(58, 132, 223);
-          -moz-box-shadow: rgba(0,0,0,0.3) 0px 1px 4px -1px;
-          -webkit-box-shadow: rgba(0, 0, 0, 0.3) 0px 1px 4px -1px;
-          box-shadow: rgba(0, 0, 0, 0.3) 0px 1px 4px -1px;
-        }
-        .#{ opts.wrapClass } {
-          display: inline-block;
-        }
-        .#{ opts.wrapClass } iframe {
-          position: relative;
-          top: 0;
-          left: 0;
-        }"
 
       ### iframe Map Object ###
       iframeObject = $(@)
 
-      ### Creates overlay object ###
-      overlayObject =
-        $("<div class=\"#{ opts.overlayClass }\">
-        <p>#{ opts.overlayMessage }</p></div>")
+      Log = (message) ->
+        if opts.printLog
+          dateTime = new Date()
+          nowTime =
+            dateTime.getHours() + ":" +
+            dateTime.getMinutes() + ":" +
+            dateTime.getSeconds()
 
-      wrapObject =
-        $("<div class=\"#{ opts.wrapClass }\"></div>")
+          console.log "mapScrollPrevent [#{nowTime}] : #{message}"
 
       ### Early exit ###
       unless iframeObject.length
-        return
+        return Log "No Iframes detected. Try changing your \"selector.\""
+      else
+        Log "#{iframeObject.length} iFrames detected."
+        mapCSS = "
+          /* --- mapScrollPrevent.js CSS Classes --- */
+          .#{ opts.overlayClass } {
+            position: absolute;
+            overflow:hidden;
+            cursor: pointer;
+            text-align: center;
+            background-color: rgba(255, 255, 255, 0);
+          }
+          .#{ opts.overlayClass },
+          .#{ opts.overlayClass } p {
+            -moz-transition: all .3s ease-in-out;
+            -o-transition: all .3s ease-in-out;
+            -webkit-transition: all .3s ease-in-out;
+            transition: all .3s ease-in-out;
+          }
+          .#{ opts.overlayClass }:hover {
+            background-color: rgba(255, 255, 255, 0.8);
+          }
+          .#{ opts.overlayClass } p {
+            font-family: Lato, 'Helvetica Neue', Helvetica, Arial, sans-serif;
+            font-size: 13px;
+            padding-top: 2.5%;
+            padding-bottom: 2.5%;
+            margin-right: auto;
+            margin-left: auto;
+            width: 70%;
+            position: relative;
+            top: 50%;
+            transform: translateY(-50%);
+            border-color: rgba(0, 0, 0, 0.3);
+            color: rgba(58, 132, 223, 0);
+            background-color: rgba(0, 0, 0, 0);
+            -moz-border-radius-topleft: 2px;
+            -webkit-border-top-left-radius: 2px;
+            border-top-left-radius: 2px;
+            -moz-border-radius-topright: 2px;
+            -webkit-border-top-right-radius: 2px;
+            border-top-right-radius: 2px;
+          }
+          .#{ opts.overlayClass }:hover p {
+            background-color: rgb(255, 255, 255);
+            color: rgb(58, 132, 223);
+            -moz-box-shadow: rgba(0,0,0,0.3) 0px 1px 4px -1px;
+            -webkit-box-shadow: rgba(0, 0, 0, 0.3) 0px 1px 4px -1px;
+            box-shadow: rgba(0, 0, 0, 0.3) 0px 1px 4px -1px;
+          }
+          .#{ opts.wrapClass } {
+            display: inline-block;
+          }
+          .#{ opts.wrapClass } iframe {
+            position: relative;
+            top: 0;
+            left: 0;
+          }"
 
-      ### Wraps the iframe ###
-      wrapIframe = ->
-        ### Check first if the iframe is already wraped ###
-        unless iframeObject.closest(".#{ opts.wrapClass }").is "div"
-          iframeObject.wrap wrapObject
-
-        ### Update variable objects with DOM objects ###
-        wrapObject =
-          iframeObject
-            .closest ".#{ opts.wrapClass }"
-            .append overlayObject
-
+        ### Creates overlay object ###
         overlayObject =
+          $("<div class=\"#{ opts.overlayClass }\">
+          <p>#{ opts.overlayMessage }</p></div>")
+
+        wrapObject =
+          $("<div class=\"#{ opts.wrapClass }\"></div>")
+
+        ### Wraps the iframe ###
+        wrapIframe = ->
+          ### Check first if the iframe is already wraped ###
+          unless iframeObject.closest(".#{ opts.wrapClass }").is "div"
+            iframeObject.wrap wrapObject
+            Log "Iframe isn't wraped."
+
+          ### Update variable objects with DOM objects ###
+          wrapObject =
+            iframeObject
+              .closest ".#{ opts.wrapClass }"
+              .append overlayObject
+
+          overlayObject =
+            wrapObject
+              .children ".#{ opts.overlayClass }"
+
+          coverObject()
+          Log "Iframe now wraped."
+
+        ### Apply all the css ###
+        applyCss = ->
+          $("head").append "<style rel=\"stylesheet\" type=\"text/css\">
+          #{ mapCSS }</style>"
+          Log "Plugin css applied."
+
+        coverObject = ->
+          overlayObject
+            .height iframeObject.height()
+            .width iframeObject.width()
+            .css
+              "top": iframeObject.position().top
+              "left": iframeObject.position().left
+          Log "Overlay positioned."
+
+        ### Overlay functions ###
+        hideOverlay = ->
+          iframeObject.css "pointer-events":"auto"
+          overlayObject.hide()
+          Log "Overlay is hidden."
+
+        showOverlay = ->
+          iframeObject.css "pointer-events":"none"
+          overlayObject.show()
+          Log "Overlay is showed."
+
+        ### Check TouchScreen ###
+        isTouchScreen = ->
+          if $(window).bind "touchstart"
+            Log "Is a touch screen."
+            true
+
+        ### Bind Events ###
+        bindEvents = ->
+          ### Dynamic Adjust ###
+          $(window)
+            .on "resize", coverObject
+
+          iframeObject
+            .on "resize", coverObject
+
+          overlayObject
+            .bind "click", hideOverlay
+
           wrapObject
-            .children ".#{ opts.overlayClass }"
+            .bind "mouseleave", hideOverlay
+            .bind "mouseenter", showOverlay
+          Log "Events bounded."
 
-        coverObject()
+        ### Init wrap and bind events ###
+        start : ->
+          Log "Starting plugin..."
+          applyCss()
+          wrapIframe()
+          bindEvents()
+          Log "Plugin Started."
 
-      ### Apply all the css ###
-      applyCss = ->
-        $("head").append "<style rel=\"stylesheet\" type=\"text/css\">
-        #{ mapCSS }</style>"
+        ### Removes everything ###
+        stop : ->
+          Log "Stopping plugin..."
+          iframeObject.removeAttr "style"
+          if iframeObject.parent().is ".#{ opts.wrapClass }"
+            iframeObject.unwrap()
 
-      coverObject = ->
-        overlayObject
-          .height iframeObject.height()
-          .width iframeObject.width()
-          .css
-            "top": iframeObject.position().top
-            "left": iframeObject.position().left
-
-      ### Overlay functions ###
-      hideOverlay = ->
-        coverObject()
-        iframeObject.css "pointer-events":"auto"
-        overlayObject.hide()
-
-      showOverlay = ->
-        coverObject()
-        iframeObject.css "pointer-events":"none"
-        overlayObject.show()
-
-      ### Check TouchScreen ###
-      isTouchScreen = ->
-        if $(window).bind "touchstart" then true
-
-      ### Init wrap and bind events ###
-      start : ->
-        applyCss()
-        wrapIframe()
-
-        ### Dynamic Adjust ###
-        $(window)
-          .on "resize", coverObject
-
-        iframeObject
-          .on "resize", coverObject
-
-        overlayObject
-          .bind "click", hideOverlay
-
-        wrapObject
-          .bind "mouseleave", hideOverlay
-          .bind "mouseenter", showOverlay
-
-      ### Removes everything ###
-      stop : ->
-        iframeObject.removeAttr "style"
-        if iframeObject.parent().is ".#{ opts.wrapClass }"
-          iframeObject.unwrap()
-
-        $(".#{ opts.overlayClass }").remove()
+          $(".#{ opts.overlayClass }").remove()
+          Log "Plugin Stopped."
